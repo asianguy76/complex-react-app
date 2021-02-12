9e00f127f6fc6ca982e4a4cb24a6c1e8e2edaf70
Khai <takecareofknives@gmail.com>
2021-02-11 20:53:20 -0600

    Added Footer, Header, HomeGuest components.

--- app/Main.js
+++ app/Main.js
@@ -1,17 +1,26 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function ExampleComponent() {
-  return (
-    <div>
-      <h1>This is our app!!!</h1>
-      <p>The sky is blue and the grass is green.</p>
-    </div>
-  )
+// Components
+
+import Header from './components/Header';
+import HomeGuest from './components/HomeGuest';
+import Footer from './components/Footer';
+
+function Main() {
+	return (
+		<>
+			<Header />
+
+			<HomeGuest />
+
+			<Footer />
+		</>
+	);
 }
 
-ReactDOM.render(<ExampleComponent />, document.querySelector("#app"))
+ReactDOM.render(<Main />, document.querySelector('#app'));
 
 if (module.hot) {
-  module.hot.accept()
+	module.hot.accept();
 }
---
+++ app/components/HomeGuest.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+function HomeGuest() {
+	return (
+		<div className="container py-md-5">
+			<div className="row align-items-center">
+				<div className="col-lg-7 py-3 py-md-5">
+					<h1 className="display-3">Official website of the Boba Boyz Gaming Collective</h1>
+					<p className="lead text-muted">A place to do stuff and things.</p>
+				</div>
+				<div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
+					<form>
+						<div className="form-group">
+							<label htmlFor="username-register" className="text-muted mb-1">
+								<small>Username</small>
+							</label>
+							<input id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
+						</div>
+						<div className="form-group">
+							<label htmlFor="email-register" className="text-muted mb-1">
+								<small>Email</small>
+							</label>
+							<input id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
+						</div>
+						<div className="form-group">
+							<label htmlFor="password-register" className="text-muted mb-1">
+								<small>Password</small>
+							</label>
+							<input id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
+						</div>
+						<button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
+							Sign up for Boba Boyz
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default HomeGuest;
